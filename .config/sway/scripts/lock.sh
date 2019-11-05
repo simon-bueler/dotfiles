151cdfb0b15c7960924d7b141e7148b7415d38ba
Simon Büeler <simon.bueler@toradex.com>
2019-11-05 16:59:27 +0100

    adjusted lock script after testing

--- .config/sway/scripts/lock.sh
+++ .config/sway/scripts/lock.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 # First, try to lock screen through logind.
 loginctl lock-session
-echo "Start"
 # logind may fail if this script will be ran from gnome-terminal when GNOME
 # session is managed by systemd. Instead, try to use d-bus method provided by
 # gnome-shell or gnome-screensaver (the latter is in GNOME Flashback).
-#if [[ $? != 0 ]]; then
+if [[ $? != 0 ]]; then
     gdbus call \
        --session \
        --object-path /org/gnome/ScreenSaver \
        --dest org.gnome.ScreenSaver \
        --method org.gnome.ScreenSaver.Lock \
        > /dev/null
-    echo "in 1st if"
 
 # If screen was locked by logind, wait a moment. Without this, screen image
 # visible before locking may be cached on user TTY which may violate user privacy
 # if someone try to switch to that TTY. This is not needed with second method
 # because `gdbus` command will wait until work is finished.
-#else
+else
     sleep 0.4
-#fi
+fi
 
 # Then, try to switch GDM's login screen. If login screen uses Xorg or if greeter
 # session was not killed yet (GDM may kill its greeter if Wayland is used,
@@ -31,13 +29,12 @@ loginctl activate \
 # If GDM killed its greeter it's needed to call internal GDM's d-bus method to
 # start new greeter session. In this case, GDM will automatically switch to proper
 # TTY. This *won't work* if greeter session is already alive.
-#if [[ $? != 0 ]]; then
+if [[ $? != 0 ]]; then
     gdbus call \
         --system \
         --object-path /org/gnome/DisplayManager/LocalDisplayFactory \
         --dest org.gnome.DisplayManager \
         --method org.gnome.DisplayManager.LocalDisplayFactory.CreateTransientDisplay \
         > /dev/null
-#fi
+fi
 sleep 4.0
-echo "End"
